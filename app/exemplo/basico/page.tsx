061606e53c15b08e3f26c32721ba2096d0d7122d
jorgetessmann-ienh <jorgetessmann@ienh.com.br>
2026-08-18 00:34:34 +0000

    dasd

---
+++ app/exemplo/basico/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/button"
+import Input from "@/components/input"
+
+export default function Home() {
+
+    return (
+        <div>
+            <Button cor="white" corText="blue" corBorda="blue"> Um Botão</Button>
+            <Input />
+        </div>
+    );
+
+
+}
