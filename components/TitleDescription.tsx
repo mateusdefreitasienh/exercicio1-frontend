b57373a728c34792c4b1976f6202cee4869145bb
leonardogoncalves-ienh <leonardogoncalves@ienh.com.br>
2026-08-24 17:52:40 +0000

    correção TitleDescription

--- components/TitleDescription.tsx
+++ components/TitleDescription.tsx
@@ -1,3 +1,35 @@
-export default function TitleDescription(props) {
-    return (<TitleDescription title="Titulo aqui" description="Descrição aqui" />)
+export default function TitleDescription(props: {
+    title: string;
+    description: string;
+}) {
+    return(
+        <div 
+        style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "12p",
+            width: "100%",
+        }}
+        >
+            <h1
+            style={{
+                fontSize: "42px",
+                fontWeight: "400px",
+                margin: "0",
+            }}
+            >
+                {props.title} 
+            </h1>
+
+            <p 
+            stule={{
+                fontSize: "18px",
+                color: "gray",
+                margin: "0"
+            }}
+            >
+                {props.description}
+            </p>
+        </div>
+    );
 }
