ce8c647d2975489b763d5041f9b42fabc9b730b7
leonardogoncalves-ienh <leonardogoncalves@ienh.com.br>
2026-08-24 18:20:52 +0000

    Correção tamanhos PersonalData

--- components/PersonalData.tsx
+++ components/PersonalData.tsx
@@ -9,72 +9,72 @@ export default function PersonalData(props) {
                 <input type="text" placeholder="Nome"
                 style={{
                     width: "100%",
-                    height: "168px",
+                    height: "40px",
                     border: "3px solid",
                     borderRadius: "10px",
                     padding: "0 48px",
-                    fontSize: "48px",
+                    fontSize: "16px",
                     outline: "none",
                 }} />
                 <p
                 style={{
                     margin: "16px 0 0 40px",
-                    fontSize: "36px",
-                }}>O erro ocoerre aqui</p>
+                    fontSize: "12px",
+                }}>O erro ocorre aqui</p>
             </div>
 
             <div style={{marginBottom: "30px"}}>
                 <input type="text" placeholder="Sobrenome"
                 style={{
                     width: "100%",
-                    height: "168px",
+                    height: "40px",
                     border: "3px solid",
                     borderRadius: "10px",
                     padding: "0 48px",
-                    fontSize: "48px",
+                    fontSize: "16px",
                     outline: "none",
                 }} />
                 <p
                 style={{
                     margin: "16px 0 0 40px",
-                    fontSize: "36px",
-                }}>O erro ocoerre aqui</p>
+                    fontSize: "12px",
+                }}>O erro ocorre aqui</p>
             </div>
 
             <div style={{marginBottom: "30px"}}>
                 <input type="email" placeholder="Email"
                 style={{
                     width: "100%",
-                    height: "168px",
+                    height: "40px",
                     border: "3px solid",
                     borderRadius: "10px",
                     padding: "0 48px",
-                    fontSize: "48px",
+                    fontSize: "16px",
                     outline: "none",
                 }} />
                 <p
                 style={{
                     margin: "16px 0 0 40px",
-                    fontSize: "36px",
-                }}>O erro ocoerre aqui</p>
+                    fontSize: "12px",
+                }}>O erro ocorre aqui</p>
             </div>
 
             <div style={{marginBottom: "30px"}}>
                 <input type="text" placeholder="Celular"
                 style={{
                     width: "100%",
-                    height: "168px",
+                    height: "40px",
                     border: "3px solid",
                     borderRadius: "10px",
                     padding: "0 48px",
-                    fontSize: "48px",
+                    fontSize: "16px",
                     outline: "none",
                 }} />
                 <p
                 style={{
                     margin: "16px 0 0 40px",
-                    fontSize: "36px",
-                }}>O erro ocoerre aqui</p>
+                    fontSize: "12px",
+                }}>O erro ocorre aqui</p>
             </div>
 
             
@@ -84,7 +84,7 @@ export default function PersonalData(props) {
                     backgroundColor: "white",
                     border: "3px solid",
                     borderRadius: "9px",
-                    fontSize: "40px"
+                    fontSize: "20px"
 
                 }}>Botao</button>
 
